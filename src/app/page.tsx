1c08b2dc3bcf494248fe6dc5858f8f3c9bac17a1
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-07-15 23:31:34 -0300

    fix:responsive home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
-      <main className="flex flex-col gap-5 items-center">
-        <h1 className="text-4xl text-white font-semibold">Bem vindo recrutador - meu dessafio </h1>
+      <main className="flex flex-col gap-5 items-center justify-center p-10">
+        <h1 className="text-4xl text-white font-semibold text-center">Bem vindo recrutador - meu dessafio </h1>
         <Link href="/users">
           <Button className="cursor-pointer">Clique aqui para ser redirecionado</Button>
         </Link>
