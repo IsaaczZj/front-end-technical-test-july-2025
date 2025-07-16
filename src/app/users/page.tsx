650b42fcc8a66a30b0e7eb18aaf88c4516be3471
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-07-15 23:22:38 -0300

    fix:making the layout responsive

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -86,7 +86,7 @@ export default function Users() {
 
   return (
     <div className="h-screen w-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 flex items-center justify-center overflow-hidden">
-      <div className="max-w-7xl flex flex-col items-center justify-center  p-10 sm:min-w-[800px] px-10">
+      <div className="max-w-2xl md:max-w-7xl flex flex-col items-center justify-center  p-10 sm:min-w-[800px] px-10">
         <div className="w-full flex flex-col gap-4 p-30">
           <header className="text-center">
             <h1 className="text-4xl text-white font-semibold">Usuários</h1>
@@ -178,7 +178,7 @@ export default function Users() {
               </Dialog>
             </form>
           </section>
-          <section className="bg-white/50 p-10 rounded-xl overflow-y-scroll max-h-[600px]">
+          <section className="bg-white/50 p-6 md:p-10 rounded-xl overflow-y-scroll max-h-[600px]">
             {users.isPending ? (
               <Loading />
             ) : (
@@ -186,21 +186,25 @@ export default function Users() {
                 <Link
                   href=""
                   key={user.id}
-                  className="p-6 flex gap-4 items-center bg-white/50 rounded-lg shadow-lg hover:scale-105 transition duration-200 mb-4"
+                  className="p-6 flex gap-4 items-center bg-white/50 rounded-lg shadow-lg hover:scale-105 transition duration-200 mb-4 "
                 >
                   <div className="p-4 rounded-full bg-white">
                     <User2Icon color="#550366" />
                   </div>
                   <div className="flex flex-col gap-2">
-                    <div className="text-2xl font-semibold">{user.name}</div>
+                    <h3 className="text-lg md:text-2xl font-semibold">
+                      {user.name}
+                    </h3>
                     <div className="flex gap-2 items-center">
                       <MailCheckIcon />
-                      <span className="text-xl">{user.email}</span>
+                      <span className="text-[12px] font-semibold md:text-xl flex ">
+                        {user.email}
+                      </span>
                     </div>
-                    <div className="flex gap-2 items-center">
+                    <p className="flex gap-2 items-center">
                       <MapPin />
                       <span>{user.address.city}</span>
-                    </div>
+                    </p>
                   </div>
                 </Link>
               ))
